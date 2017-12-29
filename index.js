8954623efccd5c13a490c34252163ce1e15bd4cf
Anirudh <anirudhbs96@gmail.com>
2017-12-29 11:02:07 +0530

    Add stringToHex function

--- index.js
+++ index.js
@@ -4,15 +4,37 @@ const client = dgram.createSocket('udp4')
 const PORT = 53
 const HOST = '8.8.8.8'
 const buf1 = Buffer.from('26f50100000100000000000014746865626573746d6f746865726675636b696e6707776562736974650000010001', 'hex')
-// const buf2 = Buffer.from('World!')
 
 client.send(buf1, PORT, HOST, (err) => {
   if (err) throw err
-  console.log('UDP message sent to', HOST, ':', PORT)
+  console.log('sent')
 })
 
-client.on('message', (msg) => {
+client.on('message', (msg, cb) => {
   console.log(msg.toString('hex'))
+  console.log(hexToString(msg.toString('hex')))
+  closeIt()
 })
 
-// client.close()
+function closeIt () {
+  console.log('closing')
+  client.close()
+}
+
+function hexToString (hex) {
+  const arr = []
+  hex.match(/.{2}/g).map((cur) => {
+    arr.push(String.fromCharCode(parseInt(cur, 16)))
+  })
+  return arr.join('')
+}
+
+function stringToHex (string) {
+  const hex = []
+  string.match(/.{1}/g).map((cur) => {
+    const h = cur.charCodeAt().toString(16)
+    if (h.length === 1) hex.push('0' + h)
+    else hex.push(h)
+  })
+  return hex.join('')
+}
