7c37872a7809fd824ed02b1b6d47b54c51618abb
Anirudh <anirudhbs96@gmail.com>
2017-12-29 11:37:45 +0530

    Add makeQuery function

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const dgram = require('dgram')
 const client = dgram.createSocket('udp4')
-
 const PORT = 53
 const HOST = '8.8.8.8'
-const buf1 = Buffer.from('26f50100000100000000000014746865626573746d6f746865726675636b696e6707776562736974650000010001', 'hex')
 
-client.send(buf1, PORT, HOST, (err) => {
-  if (err) throw err
-  console.log('sent')
-})
+function sendQuery (buf) {
+  client.send(buf, PORT, HOST, (err) => {
+    if (err) throw err
+    console.log('sent')
+  })
+}
 
 client.on('message', (msg, cb) => {
-  console.log(msg.toString('hex'))
-  console.log(hexToString(msg.toString('hex')))
+  console.log('res', msg.toString('hex'))
   closeIt()
 })
 
@@ -38,3 +37,26 @@ function stringToHex (string) {
   })
   return hex.join('')
 }
+
+function makeQuery (site) {
+  const splitup = site.split('.')
+  const hexString = []
+  splitup.map((cur) => {
+    const length = cur.length.toString(16)
+    if (length.length === 1) hexString.push('0' + length)
+    else hexString.push(length)
+
+    hexString.push(stringToHex(cur))
+  })
+  hexString.push('00') // for the last . in url
+  hexString.push('0001') // type
+  hexString.push('0001') // class
+  return hexString.join('')
+}
+
+const header = '26f501000001000000000000'
+// const query = '14746865626573746d6f746865726675636b696e6707776562736974650000010001'
+const query = makeQuery('thebestmotherfucking.website')
+const buf1 = Buffer.from(header + query, 'hex')
+
+sendQuery(buf1)
