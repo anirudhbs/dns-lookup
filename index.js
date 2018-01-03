40efd805fd66fc364f5cc29ba1c755f8ed99fa88
Anirudh <anirudhbs96@gmail.com>
2018-01-03 09:35:04 +0530

    Add giveID function for transaction IDs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = dgram.createSocket('udp4')
 const PORT = 53
 const HOST = '8.8.8.8'
 
-function sendQuery (buf) {
+function sendQuery(buf) {
   client.send(buf, PORT, HOST, (err) => {
     if (err) throw err
     console.log('sent request')
@@ -12,15 +12,17 @@ function sendQuery (buf) {
 
 client.on('message', (msg, cb) => {
   console.log('res', msg.toString('hex'))
+  console.log(hexToString(msg.toString('hex')))
   closeIt()
 })
 
-function closeIt () {
-  console.log('closing socket')
-  client.close()
+function closeIt() {
+  client.close(() => {
+    console.log('closed socket')
+  })
 }
 
-function hexToString (hex) {
+function hexToString(hex) {
   const arr = []
   hex.match(/.{2}/g).map((cur) => {
     arr.push(String.fromCharCode(parseInt(cur, 16)))
@@ -28,45 +30,69 @@ function hexToString (hex) {
   return arr.join('')
 }
 
-function stringToHex (string) {
+function stringToHex(string) {
   const hex = []
   string.match(/.{1}/g).map((cur) => {
     const h = cur.charCodeAt().toString(16)
-    if (h.length === 1) hex.push('0' + h)
-    else hex.push(h)
+    hex.push(getHex(h))
   })
   return hex.join('')
 }
 
-function makeQuery (url, queryType) {
+function makeQuery(url, queryType) {
   const splitUp = url.split('.')
   const hexString = []
   splitUp.map((cur) => {
-    const length = cur.length.toString(16)
-    if (length.length === 1) hexString.push('0' + length)
-    else hexString.push(length)
-
+    hexString.push(getHex(cur.length.toString(16)))
     hexString.push(stringToHex(cur))
   })
-  hexString.push('00') // for the last . in url
+  hexString.push('00') // last . in url
   hexString.push(getType(queryType)) // type
   hexString.push('0001') // class
   return hexString.join('')
 }
 
-function getType (type) {
+function getHex(h) {
+  return (h.length === 1) ? '0' + h : h
+}
+
+function getType(type) {
   switch (type) {
-    case 'ns': return '0002'
-    case 'soa': return '0006'
-    case 'cname': return '0005'
-    case 'mx': return '000f'
-    case 'txt': return '0010'
-    case 'aaaa': return '001c'
-    default: return '0001' // 'a'
+    case 'ns':
+      return '0002'
+    case 'soa':
+      return '0006'
+    case 'cname':
+      return '0005'
+    case 'mx':
+      return '000f'
+    case 'txt':
+      return '0010'
+    case 'aaaa':
+      return '001c'
+    default:
+      return '0001'
   }
 }
 
-const header = '26f501000001000000000000'
+function giveID () {
+  const string = '0123456789abcdef'
+  let id = ''
+  let i = 0
+  while (i < 4) {
+    id += string.charAt(Math.floor(Math.random() * string.length))
+    i += 1
+  }
+  return id
+}
+
+const id = giveID()
+const flags = '0100'
+const questionCount = '0001'
+const answerRRs = '0000'
+const authorityRRs = '0000'
+const additionalRRs = '0000'
+const header = id + flags + questionCount + answerRRs + authorityRRs + additionalRRs
 const query = makeQuery('google.com', 'a')
 const buf1 = Buffer.from(header + query, 'hex')
 
